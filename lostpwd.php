b3d28d6acb2bdf8500d559a8e6489569dc7ebdc0
Samuel Monroe <spat.monroe@gmail.com>
2015-04-13 14:12:16 +0200

    Implementing password recovery layout

---
+++ lostpwd.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<h1>Formulaire de récupération du mot de passe</h1>
+
+	<form name="lost" method="post" action="index.php?page=lostpwd">
+	Veuillez entrer votre adresse mail : : <br/>
+	<input type="text" name="lost_mail"/><br/>
+	<input type="submit" name="lost_submit"/><br>
+
+</form>
