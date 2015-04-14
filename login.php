2cc1b5ddbef12f01ca99b35db659b7e1fefc7d8a
Samuel Monroe <spat.monroe@gmail.com>
2015-04-14 09:49:07 +0200

    Working on login procedure

--- login.php
+++ login.php
@@ -7,7 +7,13 @@
 		<input type="text" name="log_pseudo"/><br/>
 		Mot de passe : <br/>
 		<input type="password" name="log_passwd"/><br/>
-		<input type="submit" name="sub_submit"/><br/>
+		<input type="submit" name="log_submit"/><br/>
 	</form>
 
 	<a href="index.php?page=lostpwd">Mot de passe oublié?</a><br/><br/>
+
+<?php
+	if(isset($_POST['log_submit']))
+	{
+		
+	}
