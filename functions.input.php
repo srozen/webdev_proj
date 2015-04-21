f39a934f126d7b3b53c83abd9c0d37db87736b90
Samuel Monroe <spat.monroe@gmail.com>
2015-04-21 18:06:15 +0200

    Prototypes for input validation functions

--- functions.input.php
+++ functions.input.php
@@ -3,5 +3,14 @@
  * SET OF FUNCTIONS USED TO VALIDATE INPUT *
  *******************************************/
 
+function valid_mail($mail)
+{
+
+}
+
+function valid_password($password)
+{
+
+}
 
 ?>
