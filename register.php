e99102846341187bec9604e10184738c16464a69
Samuel Monroe <spat.monroe@gmail.com>
2015-04-21 18:07:25 +0200

    Input names changed

--- register.php
+++ register.php
@@ -8,11 +8,11 @@
   <label>Vérification mot de passe : </label>
     <input autocomplete="off" type="password" name="password_check" placeholder="" required/><br/>
 
-  <label>Email : </label>
-    <input type="email" name="email" placeholder="" required/><br/>
+  <label>Adresse mail : </label>
+    <input type="email" name="mail" placeholder="" required/><br/>
 
-  <label>Vérification email : </label>
-    <input autocomplete="off" type="email" name="email_check" placeholder="" required/><br/>
+  <label>Vérification du mail : </label>
+    <input autocomplete="off" type="email" name="mail_check" placeholder="" required/><br/>
 
     <input type="submit" name="reg_submit" value="Inscription"/>
 </form>
