854638588ac3598e6d6a400f9dd46f6085356735
Samuel Monroe <spat.monroe@gmail.com>
2015-04-22 20:40:46 +0200

    New file containing string constants

---
+++ constants.text.php
@@ -0,0 +1,4 @@
+<?php
+  $LOGINLENGTH = ' ' . ($config['LOGIN']['minlength']+1) . ' - ' . $config['LOGIN']['maxlength'] . ' caractères ';
+  $PASSWORDLENGTH = ' ' . ($config['PASSWORD']['minlength']+1) . ' - ' . $config['PASSWORD']['maxlength'] . ' caractères, une majuscule, une minuscule et un chiffre minimum';
+?>
