8ea3c5d3cb5d3e7b64283c1de94ca836bb42cb94
Samuel Monroe <spat.monroe@gmail.com>
2015-04-22 12:09:23 +0200

    New functions to compare strings, validate mail login and password

--- functions.input.php
+++ functions.input.php
@@ -3,14 +3,34 @@
  * SET OF FUNCTIONS USED TO VALIDATE INPUT *
  *******************************************/
 
+/* Check if mail is a real mail */
 function valid_mail($mail)
 {
-
+  return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
 
+/* Check if login follows requirements */
+function valid_login($login)
+{
+  return preg_match('/^[A-Za-z]{1}[A-Za-z0-9]{'. $config['LOGIN']['minlength'] . ',' . $config['LOGIN']['maxlength'] .'}$/', $login);
+}
+
+/* Check if password follows requirements */
 function valid_password($password)
 {
+  return preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{'. $config['PASSWORD']['minlength'] . ',' . $config['PASSWORD']['maxlength'] . '50}$/', $password)
+}
 
+/* Check if two strings are the same, if flag is true the comparison is case unsensitive */
+function same_inputs($str1, $str2, $flag = false)
+{
+  if($flag)
+  {
+    $str1 = strtolower($str1);
+    $str2 = strtolower($str2);
+  }
+  if (strcmp($str1, $str2) == 0) return true;
+  else return false;
 }
 
 ?>
