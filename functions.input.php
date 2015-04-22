ab75078bd960ea77f748a95b939e101275806df3
Samuel Monroe <spat.monroe@gmail.com>
2015-04-22 20:39:49 +0200

    Config in parameters in order to use it

--- functions.input.php
+++ functions.input.php
@@ -3,22 +3,28 @@
  * SET OF FUNCTIONS USED TO VALIDATE INPUT *
  *******************************************/
 
+/* Check if $var is not empty and set */
+function filled($var)
+{
+  return (isset($var) AND !empty($var));
+}
+
 /* Check if mail is a real mail */
 function valid_mail($mail)
 {
-  return filter_var($email, FILTER_VALIDATE_EMAIL);
+  return filter_var($mail, FILTER_VALIDATE_EMAIL);
 }
 
 /* Check if login follows requirements */
-function valid_login($login)
+function valid_login($login, $config)
 {
   return preg_match('/^[A-Za-z]{1}[A-Za-z0-9]{'. $config['LOGIN']['minlength'] . ',' . $config['LOGIN']['maxlength'] .'}$/', $login);
 }
 
 /* Check if password follows requirements */
-function valid_password($password)
+function valid_password($password, $config)
 {
-  return preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{'. $config['PASSWORD']['minlength'] . ',' . $config['PASSWORD']['maxlength'] . '50}$/', $password)
+  return preg_match('/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{'. $config['PASSWORD']['minlength'] . ',' . $config['PASSWORD']['maxlength'] . '50}$/', $password);
 }
 
 /* Check if two strings are the same, if flag is true the comparison is case unsensitive */
