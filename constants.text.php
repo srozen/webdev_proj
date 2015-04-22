679a8e34e13afb33cfe4d24616eb530159b6c351
Samuel Monroe <spat.monroe@gmail.com>
2015-04-23 00:06:08 +0200

    Unused file

--- constants.text.php
+++
@@ -1,4 +0,0 @@
-<?php
-  $LOGINLENGTH = ' ' . ($config['LOGIN']['minlength']+1) . ' - ' . $config['LOGIN']['maxlength'] . ' caractères ';
-  $PASSWORDLENGTH = ' ' . ($config['PASSWORD']['minlength']+1) . ' - ' . $config['PASSWORD']['maxlength'] . ' caractères, une majuscule, une minuscule et un chiffre minimum';
-?>
