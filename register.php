ffc8edd10f6984783549a6cf506c92e39d5a01dd
Samuel Monroe <spat.monroe@gmail.com>
2015-04-22 20:40:34 +0200

    Included register checking

--- register.php
+++ register.php
@@ -1,9 +1,20 @@
+<?php
+  if(isset($_POST['reg_submit']))
+  {
+    valid_register_login($_POST['login'], $config);
+    valid_register_mail($_POST['mail'], $_POST['mail_check'], $config);
+    valid_register_password($_POST['password'], $_POST['password_check'], $config);
+  }
+?>
+
 <form name="regiser" action="index.php?page=register" method="post">
   <label>Login : </label>
-    <input type="text" name="login" placeholder="" required/><br/>
+    <input type="text" name="login" required/>
+    <?php echo '<small>' . $LOGINLENGTH . '</small><br/>';?>
 
   <label>Mot de passe : </label>
-    <input type="password" name="password" placeholder="" required/><br/>
+    <input type="password" name="password" placeholder="" required/>
+    <?php echo '<small>' . $PASSWORDLENGTH . '</small><br/>';?>
 
   <label>Vérification mot de passe : </label>
     <input autocomplete="off" type="password" name="password_check" placeholder="" required/><br/>
