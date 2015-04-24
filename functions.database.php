103cb34f5caed859efdacaf6f68f8d76512b5c40
Samuel Monroe <spat.monroe@gmail.com>
2015-04-24 09:39:02 +0200

    New function to get a user value from known info

--- functions.database.php
+++ functions.database.php
@@ -4,7 +4,8 @@
  * FUNCTIONS USED TO GET INFO FROM DB *
  ************************************** /
 
-/* Returns boolean, check if login is in database */
+/* Returns boolean, check if $field is already in database */
+// USE : user_exists('id', 1, $dbsocket); "Is the id 1 in user table?" //
 function user_exists($field, $value, $dbsocket)
 {
   $query = 'SELECT count(*)
@@ -21,4 +22,18 @@ function user_exists($field, $value, $dbsocket)
   {
     return false;
   }
+}
+
+/* Return a user value from a known $col $colvalue */
+// USE : get_user_value('login', 'id', '1', $dbsocket); "Get me the login value for id 1" //
+function get_user_value($value, $col, $colvalue, $dbsocket)
+{
+  $query = 'SELECT ' . $value . '
+            FROM user
+            WHERE ' . $col . ' = \'' . $colvalue . '\';';
+
+  $result = $dbsocket->qiery($query);
+  $uservalue = $result->fetch();
+
+  return $uservalue[$value];
 }
