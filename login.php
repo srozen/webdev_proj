2f64f489b2e738b21c34d17d4a43b664ec8c2a98
Samuel Monroe <spat.monroe@gmail.com>
2015-04-24 14:52:59 +0200

    Simple login form

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+
+	<form name="login" method="post" action="index.php?page=login">
+		Pseudo : <br/>
+		<input type="text" name="login"/><br/>
+		Mot de passe : <br/>
+		<input type="password" name="password"/><br/>
+		<input type="submit" name="submit"/><br/>
+	</form>
+
+	<a href="index.php?page=lostpwd">Mot de passe oublié?</a><br/><br/>
