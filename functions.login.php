53035d2da108c01fa872c35be673151eb4dee9c4
Samuel Monroe <spat.monroe@gmail.com>
2015-04-25 18:33:27 +0200

    Login & activation procedure is now almost finished

--- functions.login.php
+++ functions.login.php
@@ -10,23 +10,26 @@ function login($login, $password, $config, $dbsocket, $code = '0')
 
     $result = $dbsocket->query($query);
 
-    $user = $result->fetch();
+    $user = $result->fetch(PDO::FETCH_ASSOC);
 
     // If request returned a result //
-    if(/!empty($user))
+    if(!empty($user))
     {
       // Check if user is activating //
       if($user['subclass'] == 'activating')
       {
         if($code != 0)
         {
-          if(/* Code fit in db */)
+          if(get_activation_code($user['id']) == $code)
           {
             // Activation //
             // Changement de status user
+            //set_user_value('subclass', 'normal', $user['id'], $dbsocket);
             // Chargement de la session
-
+            // $_SESSION['logged'] = true;
+            // $_SESSION['user'] = new User();
             // Set date d'activation
+            //set_user_value('activation', 'NOW()', $user['id'], $dbsocket);
             // Redirection vers profil ou index
           }
           else
@@ -57,4 +60,5 @@ function login($login, $password, $config, $dbsocket, $code = '0')
   }
 
 }
+
 ?>
