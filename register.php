e739b1186559f3e31de31c9d86086e149d895095
Samuel Monroe <spat.monroe@gmail.com>
2015-04-25 10:00:37 +0200

    Layout changes in the html

--- register.php
+++ register.php
@@ -19,12 +19,12 @@
     if($register_log['valid'])
     {
       echo '<span> Les données user suivantes vont être encodées en base de donnée : </span></br><pre>';
-      print_r(create_new_user($_POST['login'], $_POST['password'], $_POST['mail'], 'activating', $config, $dbsocket));
+      print_r(create_new_user($_POST['login'], $_POST['password'], $_POST['mail'], $config, $dbsocket));
       echo '</pre>';
       // Generate activation code
         $code = generate_activation_code($_POST['mail'], $_POST['login']);
       // Create new user
-      
+
       // Add activation link into db
 
       // Send email
@@ -35,30 +35,30 @@
 
 <?php echo $register_log['message']; ?>
 <form name="regiser" action="index.php?page=register" method="post">
-  <label>Login : </label>
+  <label>Login : </label><br/>
     <input type="text" value ="<?php echo $register_log['login']; ?>" class="<?php echo $register_log['loginclass']; ?>" name="login" />
     <?php
       echo '<small>' . $LOGIN_REQUIREMENTS . '</small><br/>';
       echo $register_log['loginmessage'];
     ?>
 
-  <label>Mot de passe : </label>
+  <label>Mot de passe : </label><br/>
     <input type="password" class="<?php echo $register_log['passwordclass']; ?>" name="password" placeholder="" />
     <?php
       echo '<small>' . $PASSWORD_REQUIREMENTS . '</small><br/>';
       echo $register_log['passwordmessage'];
     ?>
 
-  <label>Vérification mot de passe : </label>
+  <label>Vérification mot de passe : </label><br/>
     <input autocomplete="off" type="password" class="<?php echo $register_log['passwordclass']; ?>" name="checkpassword" placeholder="" /><br/>
 
-  <label>Adresse mail : </label>
+  <label>Adresse mail : </label><br/>
     <input type="text" value="<?php echo $register_log['mail']; ?>" class="<?php echo $register_log['mailclass']; ?>" name="mail" placeholder="" /><br/>
     <?php
       echo $register_log['mailmessage'];
     ?>
 
-  <label>Vérification du mail : </label>
+  <label>Vérification du mail : </label><br/>
     <input autocomplete="off" type="text" class="<?php echo $register_log['mailclass']; ?>" name="checkmail" placeholder="" /><br/>
 
     <input type="submit" name="reg_submit" value="Inscription"/>
