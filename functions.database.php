da0df9ad7407afcade904d591c316295694cee68
Samuel Monroe <spat.monroe@gmail.com>
2015-04-25 18:33:06 +0200

    New function to update user field

--- functions.database.php
+++ functions.database.php
@@ -1,8 +1,9 @@
 <?php
 
-/**************************************
- * FUNCTIONS USED TO GET INFO FROM DB *
- ************************************** /
+/********************************************
+ * FUNCTIONS USED TO INTERACT WITH DATABASE *
+ ********************************************/
+
 
 /* Returns boolean, check if $field is already in database */
 // USE : user_exists('id', 1, $dbsocket); "Is the id 1 in user table?" //
@@ -32,8 +33,33 @@ function get_user_value($value, $col, $colvalue, $dbsocket)
             FROM user
             WHERE ' . $col . ' = \'' . $colvalue . '\';';
 
-  $result = $dbsocket->qiery($query);
+  $result = $dbsocket->query($query);
   $uservalue = $result->fetch();
 
   return $uservalue[$value];
+}
+
+/* Update a targeted user from a $userid with designated $field with a $value */
+// USE : set_user_value('login', 'johnsmith', '24', $dbsocket); "Set user 24 login with 'johnsmith'" //
+function set_user_value($field, $value, $userid, $dbsocket)
+{
+  $query = 'UPDATE user
+            SET ' . $field . ' =\'' . $value . '\'
+            WHERE user_id =\'' . $userid . '\';';
+
+  $result = $dbsocket->exec($query);
+  return $result;
+}
+
+/* Return an activation code from a $userid */
+// USER : get_activation_code(1, $dbsocket); "Get me the activation code for user 1" //
+function get_activation_code($userid, $dbsocket)
+{
+  $query = 'SELECT code
+            FROM activation
+            WHERE user_id = \'' . $userid . '\';';
+  $result = dbsocket->query($query);
+  $activation = $result->fetch();
+
+  return $activation['code'];
 }
