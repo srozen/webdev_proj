6998a2483bbd1a36eeead76e04e77f923776ec1e
Samuel Monroe <spat.monroe@gmail.com>
2015-04-26 13:35:05 +0200

    Included login function

--- login.php
+++ login.php
@@ -1,5 +1,15 @@
 <?php
 
+	if(logged())
+	{
+	  header("Location: index.php");
+	  die();
+	}
+
+	if(isset($_POST['login_submit']))
+	{
+		login($_POST['login'], $_POST['password'], $_GET['activation'], $config, $dbsocket);
+	}
 ?>
 
 
@@ -8,7 +18,7 @@
 		<input type="text" name="login"/><br/>
 		Mot de passe : <br/>
 		<input type="password" name="password"/><br/>
-		<input type="submit" name="submit"/><br/>
+		<input type="submit" name="login_submit"/><br/>
 	</form>
 
 	<a href="index.php?page=lostpwd">Mot de passe oublié?</a><br/><br/>
