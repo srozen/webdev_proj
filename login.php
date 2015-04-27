e7e44038cb942fada0de5c39e0f31055e836f18c
Samuel Monroe <spat.monroe@gmail.com>
2015-04-27 12:28:57 +0200

    Added the hidden field as required in the set of specs files

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
 
 	if(isset($_POST['login_submit']))
 	{
-		login($_POST['login'], $_POST['password'], $_GET['activation'], $config, $dbsocket);
+		login($_POST['login'], $_POST['password'], $_POST['activation'], $config, $dbsocket);
 	}
 ?>
 
@@ -18,6 +18,7 @@
 		<input type="text" name="login"/><br/>
 		Mot de passe : <br/>
 		<input type="password" name="password"/><br/>
+		<input type="hidden" name="activation" value="<?php echo $_GET['activation']; ?>"/>
 		<input type="submit" name="login_submit"/><br/>
 	</form>
 
