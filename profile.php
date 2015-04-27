2ba46711864b6a99b3cc10b190cf25debb09ec55
Samuel Monroe <spat.monroe@gmail.com>
2015-04-27 16:08:11 +0200

    Start of profile page

--- profile.php
+++ profile.php
@@ -1,9 +1,30 @@
 <?php
 
-if(!logged())
-{
-  header("Location: index.php");
-  die();
-}
+  if(!logged())
+  {
+    header("Location: index.php");
+    die();
+  }
 
 ?>
+
+<h3> Données du profil </h3>
+<pre>
+  <h4> Avatar : </h4>
+  <h4> Login : </h4>
+  <h4> Mail : </h4>
+</pre>
+
+<h3> Modification du profil </h3>
+
+Modifier votre : <a href="index.php?page=profile&modification=avatar"> Avatar </a>
+                 <a href="index.php?page=profile&modification=login"> Login </a>
+                 <a href="index.php?page=profile&modification=mail"> Mail </a>
+                 <a href="index.php?page=profile&modification=password"> Mot de passe </a>
+
+<?php
+
+  if(isset($_GET['modification']))
+  {
+    //
+  }
