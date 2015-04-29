2296f6685e16deb200732d0f1ecb76e8fbb22238
Samuel Monroe <spat.monroe@gmail.com>
2015-04-29 16:18:57 +0200

    Created bases of administration page

---
+++ administration.php
@@ -0,0 +1,22 @@
+<?php
+
+  if(!logged())
+  {
+    header("Location: index.php");
+    die();
+  }
+  /*
+  if(!is_admin($_SESSION['user'])
+  {
+    header("Location: index.php");
+    die();
+  }
+  */
+
+  echo '<ul> Panneau d\'administration';
+  echo '<li><a href=index.php?page=administration&manage=user> Gestion des membres </a></li>';
+  echo '<li><a href=index.php?page=administration&manage=mail> Gestion des messages </a></li>';
+  echo '<li><a href=index.php?page=administration&manage=config> Gestion de la configuration </a></li>';
+  echo '</ul>';
+
+?>
