6c06c70dfeb6ba0e22b151c3371092397069a805
Samuel Monroe <spat.monroe@gmail.com>
2015-04-30 08:54:52 +0200

    Welcome message display username

--- welcome.php
+++ welcome.php
@@ -1,3 +1,3 @@
-<div>Bienvenue sur le Wiki </div>
+<h2>Bienvenue sur le Wiki, <?php if(logged()) echo $_SESSION['user']->getLogin(); else echo 'visiteur'; ?> ! </h2>
 <div>Vous pourrez bientôt vous inscrire sur le site.</div>
 <div>D'autres fonctionnalités seront implémentées par la suite.</div>
