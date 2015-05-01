742e792da92e368a7012bac01f509d8c1de3aa0b
Samuel Monroe <spat.monroe@gmail.com>
2015-05-01 16:31:58 +0200

    Function to handle contact message answers

--- functions/functions.messenger.php
+++ functions/functions.messenger.php
@@ -45,7 +45,7 @@ function confirm_contact_message($mail, $subj, $text)
 {
   $to = $mail;
 
-  $subject = 'Message envoyé avec succès';
+  $subject = 'Message envoyé : ' . $subj;
 
   $headers = "From: " . strip_tags('no-reply@wiki.pmm.be') . "\r\n";
   $headers .= "Reply-To: ". strip_tags('no-reply@wiki.pmm.be') . "\r\n";
@@ -62,5 +62,31 @@ function confirm_contact_message($mail, $subj, $text)
   $message .= '</ul></body></html>';
 
   mail($to, $subject, $message, $headers);
+}
+
+function answer_contact_message($mail, $subj, $text, $answer, $id, $dbsocket)
+{
+  $to = $mail;
+
+  $subject = 'Re : ' . $subj;
+
+  $headers = "From: " . strip_tags('no-reply@wiki.pmm.be') . "\r\n";
+  $headers .= "Reply-To: ". strip_tags('no-reply@wiki.pmm.be') . "\r\n";
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=utf-8\r\n";
+
+  $message = '<html><body>';
+  $message .= '<pre>' . $answer . '</pre>';
+  $message .= '<h3>Rappel de votre message : <h3>';
+  $message .= '<pre>' . $text . '</pre>';
+  $message .= 'Pour toute autre demande, veuillez ouvrir un nouveau ticket via la page contact du site.';
+  $message .= '</body></html>';
+
+  mail($to, $subject, $message, $headers);
+
+  $query = 'UPDATE contact_message
+            SET answer = true
+            WHERE id = \'' . $id . '\';';
+  $result = $dbsocket->exec($query);
 }
 ?>
