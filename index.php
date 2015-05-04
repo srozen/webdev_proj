9482f92f424b482df880c20d8cc6db244dae73dd
Samuel Monroe <spat.monroe@gmail.com>
2015-05-04 13:32:52 +0200

    Working index file

--- index.php
+++ index.php
@@ -4,25 +4,59 @@
    * START PAGE AND MAIN CONTAINER *
    *********************************/
 
+  // Classes inclusions before any action in order to allow $SESSION with class uses //
   include('classes/class.page.php');
 
+  // Parsing the config file
   $config = parse_ini_file('config.ini', true);
 
+  // Enabling the session
   session_name($config['SESSION']['session_name']);
   session_start();
-
-  try
-  {
-    $dbsocket = new PDO('mysql:host=localhost; dbname=' . $config['DATABASE']['dbname'] . '; charset=utf8', $config['DATABASE']['dblogin'], $config['DATABASE']['dbpassword']);
-    $dbsocket->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch(Exception $e)
-  {
-    die('Erreur : '.$e->getMessage());
-    echo 'Database connexion failed';
-  }
 
   // Include functions and classes files
   include('functions/functions.index.php');
 
-  ?>
+  // Creating the database socket
+  $dbsocket = database_socket();
+
+  // Creating the Page object
+  if(isset($_GET['page']))  $page = create_page($_GET['page']);
+  else $page = create_page('index');
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    <title><?php echo $config['GLOBAL']['title'] . ' - ' . $page->getTabTitle(); ?></title>
+  </head>
+
+  <body>
+
+    <header>
+      <img src="<?php echo $config['GLOBAL']['banner']; ?>" alt="Logo Ephec"/>
+      <h1><?php echo $config['GLOBAL']['title'];?></h1>
+      <nav><span>Menu : </span><?php create_menu(); ?> </nav>
+    </header>
+
+    <section>
+      <?php
+        echo '<h2>' . $page->getTitle() . '</h2>';
+        include($page->getFile());
+      ?>
+    </section>
+
+    <footer>
+      <span><?php echo $config['GLOBAL']['copyright'] . ' ';?><a href="mailto:<?php echo $config['ADMIN']['admin_mail'];?>"><?php echo $config['ADMIN']['admin_name'];?></a> - Code sous licence MIT</span>
+    </footer>
+
+  </body>
+
+</html>
+
+<?php
+  $dbsocket = null;
+?>
