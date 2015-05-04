4cdc59f64e6970eeec30d3d3d17f6cf8e0aa3016
Samuel Monroe <spat.monroe@gmail.com>
2015-05-04 13:32:24 +0200

    Removed unused fields

--- functions/functions.index.php
+++ functions/functions.index.php
@@ -8,9 +8,7 @@
   function create_menu()
   {
     $nav =  '<a href="index.php?page=index"> Accueil </a>';
-    $nav .= '<a href="index.php?page=contact"> Contact </a>';
     $nav .= '<a href="index.php?page=register"> Inscription </a>';
-    $nav .= '<a href="index.php?page=login"> Connexion </a>';
     echo $nav;
   }
 
@@ -21,14 +19,8 @@
       case 'index' :
         return $values = array('Accueil', 'welcome.php', 'Page d\'accueil');
         break;
-      case 'contact' :
-        return $values = array('Contact', 'contact.php', 'Page de contact');
-        break;
       case 'register' :
         return $values = array('Inscription', 'register.php', 'Page d\'inscription');
-        break;
-      case 'login' :
-        return $values = array('Connexion', 'login.php', 'Page de connexion');
         break;
       default :
         return $values = array('Accueil', 'welcome.php', 'Page d\'accueil');
@@ -39,5 +31,21 @@
   function create_page($page)
   {
     $values = page_values($page);
-    return new Page($pvalues[0], $pvalues[1], $pvalues[2]);
+    return new Page($values[0], $values[1], $values[2]);
+  }
+
+  function database_socket()
+  {
+    try
+    {
+      $dbsocket = new PDO('mysql:host=localhost; dbname=' . $GLOBALS['config']['DATABASE']['dbname'] . '; charset=utf8', $GLOBALS['config']['DATABASE']['dblogin'], $GLOBALS['config']['DATABASE']['dbpassword']);
+      $dbsocket->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $dbsocket;
+    }
+    catch(Exception $e)
+    {
+      die('Erreur : '.$e->getMessage());
+      echo 'Database connexion failed';
+    }
+
   }
