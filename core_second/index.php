aaead402e70dab44a191428b7e2be234b73ef6da
Samuel Monroe <spat.monroe@gmail.com>
2015-05-04 13:31:12 +0200

    Error

--- core_second/index.php
+++ core_second/index.php
@@ -12,17 +12,6 @@
   session_name($config['SESSION']['session_name']);
   session_start();
 
-  try
-  {
-    $dbsocket = new PDO('mysql:host=localhost; dbname=' . $config['DATABASE']['dbname'] . '; charset=utf8', $config['DATABASE']['dblogin'], $config['DATABASE']['dbpassword']);
-    $dbsocket->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch(Exception $e)
-  {
-    die('Erreur : '.$e->getMessage());
-    echo 'Database connexion failed';
-  }
-
 
   // Include functions and classes files
   include('functions/functions.index.php');
