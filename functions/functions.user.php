e4acf039657fdf714666924823f4c0699889e314
Samuel Monroe <spat.monroe@gmail.com>
2015-05-04 14:31:53 +0200

    Function to search if value exists in user

--- functions/functions.user.php
+++ functions/functions.user.php
@@ -3,4 +3,27 @@
    * USER RELATED FUNCTIONS *
    **************************/
 
-  
+  /**
+   * @param $field - Specify which column to search in
+   * @param $value - Specify value to look for
+   *
+   * @return Boolean telling if the value has been found
+  */
+
+  function user_exists()
+  {
+    $query = 'SELECT count(*)
+              FROM user
+              WHERE ' . $field . ' = \'' . $value . '\';';
+
+    $result = $dbsocket->query($query);
+
+    if($result->fetchColumn() > 0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
