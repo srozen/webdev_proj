2fdeedff61a93fd958a664460a506743ff6a5356
Samuel Monroe <spat.monroe@gmail.com>
2015-05-06 00:36:50 +0200

    Indoor authentication function

--- functions/functions.authentication.php
+++ functions/functions.authentication.php
@@ -11,4 +11,21 @@
     return(is_user_status('admin', $userid));
   }
 
+  function indoor_auth($password)
+  {
+    $query = 'SELECT count(*)
+              FROM user
+              WHERE id = \'' . $_SESSION['user']->getId() . '\' AND password = \'' . encrypt($password) . '\';';
+    $result = $GLOBALS['dbsocket']->query($query);
+
+    if($result->fetchColumn() > 0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
 ?>
