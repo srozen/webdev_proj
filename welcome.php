55ef1ec47aba376e9fd9113f8ee9a78266738c71
Samuel Monroe <spat.monroe@gmail.com>
2015-05-05 15:20:49 +0200

    Display user Object info if logged

--- welcome.php
+++ welcome.php
@@ -1,3 +1,12 @@
-<h4>Bienvenue sur le Wiki ! </<h4></h4>><br/>
+<h4>Bienvenue sur le Wiki ! </<h4></h4><br/>
 <div>Vous pourrez bientôt vous inscrire sur le site.</div>
 <div>D'autres fonctionnalités seront implémentées par la suite.</div>
+
+<?php
+  if(logged())
+  {
+    echo '<pre>';
+    print_r($_SESSION['user']);
+    echo '</pre>';
+  }
+?>
