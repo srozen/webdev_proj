ab02697a35a39b5bb36c5a2f0e33ca0693fbe45c
Samuel Monroe <spat.monroe@gmail.com>
2015-05-07 20:57:44 +0200

    Complete recovery form, functionnal

--- recoverpassword.php
+++ recoverpassword.php
@@ -8,21 +8,33 @@ if(logged())
 if(filled($_GET['activation']))
 {
   $code = $_GET['activation'];
-  $query = 'SELECT id
-            FROM user
-            WHERE id = (SELECT user_id FROM activation WHERE code = ' . $code . ' AND recovery = false):';
+  $query = 'SELECT id FROM user WHERE id = (SELECT user_id FROM activation WHERE code = \'' . $code . '\' AND recovery = false);';
   $result = $GLOBALS['dbsocket']->query($query);
   $userid = $result->fetch();
 
-  $user = new User($uservalue['id']);
+  if(empty($userid))
+  {
+    echo '<div class="error_msg"> Vous n\'êtes pas en demande de mot de passe, veuillez vous connecter normalement.</div>';
+    die();
+  }
 
+  $user = new User($userid['id']);
   $question = get_user_value('question', 'id', $user->getId());
 
-  echo '<form name=recoverpassword action="index.php?page=recoverpassword" method="post">
+  if(isset($_POST['submit']))
+  {
+    process_password_recovery($_POST['answer'], $_POST['password'], $_POST['checkpassword'], $user);
+  }
+
+  echo '<form name=recoverpassword action="index.php?page=recoverpassword&activation=' . $_GET['activation'] . '" method="post">
           <label> Votre question est : </label><br/>
           <p> ' . $question . '</p>
           <label> Réponse : </label><br/>
-          <input type="text" name="answer"/>
+            <input type="text" name="answer"/><br/>
+          <label> Nouveau mot de passe : </label></br>
+            <input type="password" name="password"/><br/>
+          <label> Vérification du mot de pass : </label><br/>
+            <input type="password" name="checkpassword"/><br/>
           <input type="submit" name="submit">
         </form>';
 }
