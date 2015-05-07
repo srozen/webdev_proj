1b5e0fb874b235fe3906ef20a18e4e61c90bd0aa
Samuel Monroe <spat.monroe@gmail.com>
2015-05-07 11:49:07 +0200

    Status checkers for right access

--- functions/functions.authentication.php
+++ functions/functions.authentication.php
@@ -8,7 +8,47 @@
 
   function admin($userid)
   {
-    return(is_user_status('admin', $userid));
+    return (is_user_status('admin', $userid));
+  }
+
+  function subadmin($userid)
+  {
+    return (is_user_status('subadmin', $userid));
+  }
+
+  function normal($userid)
+  {
+    return (is_user_status('normal', $userid));
+  }
+
+  function activating($userid)
+  {
+    return (is_user_status('activating', $userid));
+  }
+
+  function reactivating($userid)
+  {
+    return (is_user_status('reactivating', $userid));
+  }
+
+  function lostpassword($userid)
+  {
+    return (is_user_status('lostpassword', $userid));
+  }
+
+  function frozen($userid)
+  {
+    return (is_user_status('frozen', $userid));
+  }
+
+  function unregistered($userid)
+  {
+    return (is_user_status('unregistered', $userid));
+  }
+
+  function banned($userid)
+  {
+    return (is_user_status('banned', $userid));
   }
 
   function indoor_auth($password)
