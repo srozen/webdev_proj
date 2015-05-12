a9f6e09000be18e9569352abecdf402e7217b1bb
Samuel Monroe <spat.monroe@gmail.com>
2015-05-12 12:17:17 +0200

    Parser is complete

--- functions/functions.parser.php
+++ functions/functions.parser.php
@@ -2,25 +2,29 @@
 
   function parser($text)
   {
-    //$text = stripslashes($text); // On enlève les slashs qui se seraient ajoutés automatiquement
-    $text = htmlspecialchars($text); // On rend inoffensives les balises HTML que le visiteur a pu rentrer
-    $text = nl2br($text); // On crée des <br /> pour conserver les retours à la ligne
+    // Replacing special chars by html code
+    $text = htmlspecialchars($text);
 
-    // On retire les mots clés
+    // Creating <br/> to keep carriage returns
+    $text = nl2br($text);
+
+    // Removing false tags
     $text = preg_replace('#\[\[(.+)\]\]#isU', '<a href="#">$1</a>', $text);
 
-    $processed_tags = 0;
+    // Boolean value used by whole function to exit loop when no tag is processed
+    $processed_tags = true;
 
 
     $wtext = '';
     $wtag = '';
-    while($processed_tags <2)
+    while($processed_tags)
     {
       $i = 0;
+      $processed_tags = false;
       // Premier itérateur pour parcourir la chaîne entière
       for($i; $i < strlen($text); $i++)
       {
-        if($text[$i] == '[')
+        if($text[$i] == '[' AND $text[$i-1] != '\\')
         {
           // On commence le working tag
           $wtag .= $text[$i];
@@ -29,20 +33,21 @@
           for($j = $i+1; $j < strlen($text); $j++)
           {
             // Si on retombe sur un tag ouvrant; on sauve le tag supérieur et on reset le workign tag
-            if($text[$j] == '[')
+            if($text[$j] == '[' AND $text[$j-1] != '\\')
             {
               $wtext .= $wtag;
               $wtag = '';
               $wtag = $text[$j];
             }
             // Si balise fermante, on l'inclus et on parse, ensuite on recommence au dernier char lu
-            else if($text[$j] == ']')
+            else if($text[$j] == ']' AND $text[$j-1] != '\\')
             {
               $wtag .= $text[$j];
               $wtag = parser_decode($wtag);
               $wtext .= $wtag;
               $wtag = '';
               $i = $j;
+              $processed_tags = true;
               break;
             }
             // Si non balise, on continue en ajoutant le char au workign tag
@@ -57,17 +62,78 @@
           $wtext .= $text[$i];
         }
       }
-      $processed_tags++;
       $text = $wtext;
       $wtext = '';
     }
+    echo parser_specialchars($text);
+  }
 
-    echo $text;
+  function parser_decode($text)
+  {
+    /****************
+    * SPECIAL CHARS *
+    *****************/
+    // On retire les fausses balises
+    $text = preg_replace('#\[[^abipuhnd]\|(.[^\]]+)\]#', '$1', $text);
+
+    /**************
+    * SIMPLE TAGS *
+    ***************/
+    // COMMENTS
+    $text = preg_replace('#\[\!\|(.+)\]#isU', '<!--$1-->', $text);
+    // TITLES
+    $text = preg_replace('#\[(1-4)\|(.+)\]#isU', '<h$1>$2</h$1>', $text);
+    // DIVS
+    $text = preg_replace('#\[d\|(.+)\]#isU', '<div>$1</div>', $text);
+    // PARAGRAPHS
+    $text = preg_replace('#\[p\|(.+)\]#isU', '<p>$1</p>', $text);
+    // BR TAG
+    $text = preg_replace('#\[n\]#isU', '<br/>', $text);
+    // HORIZONTAL ROW
+    $text = preg_replace('#\[h\]#isU', '<hr/>', $text);
+    // BOLD
+    $text = preg_replace('#\[b\|(.+)\]#isU', '<b>$1</b>', $text);
+    // ITALIC
+    $text = preg_replace('#\[u\|(.+)\]#isU', '<u>$1</u>', $text);
+    // UNDERLINED
+    $text = preg_replace('#\[i\|(.+)\]#isU', '<i>$1</i>', $text);
+
+
+    /***************
+    * COMPLEX TAGS *
+    ****************/
+    // URL
+    $text = preg_replace('#\[\s?a\s?\|\s?(.+)\s?\|\s?(.+)\s?\]#isU', '<a href="$1">$2</a>', $text);
+    // IMAGE
+    $text = preg_replace('#\[img\s?\|\s?(.+)\s?\|\s?(.+)\s?\]#isU', '<img src="$1" alt="$2"/>', $text);
+    // SPECIAL OLS
+    $text = preg_replace('#\[ol_\|(.+)\|(.+)\|(.+)\]#isU', '<ol type="$1" start="$2"><li>$3</li></ol>', $text);
+    // NORMAL ULS
+    $text = preg_replace('#\[ul\|(.*)\]#isU', '<ul><li>$1</li></ul>', $text);
+    // NORMAL OLS
+    $text = preg_replace('#\[ol\|(.*)\]#isU', '<ol><li>$1</li></ol>', $text);
+    // COLORED TEXT SPANS
+    $text = preg_replace('#\[\#(.+)\|(.+)\]#isU', '<span style="color:#$1;">$2</span>', $text);
+    // COLORED BACKGROUNDED SPANS
+    $text = preg_replace('#\[bg\|\#(.+)\|(.+)\]#isU', '<span style="background : #$1">$2</span>', $text);
+    // COLORED BACKGROUNDED DIVS
+    $text = preg_replace('#\[div\|\#(.+)\|(.+)\]#isU', '<div style="background : #$1">$2</div>', $text);
+    // IMAGE BACKGROUNDED DIVS
+    $text = preg_replace('#\[div\|(.+)\|(.+)\]#isU', '<div style="background : url($1);">$2</div>', $text);
+    // CREATING LIST ITEMS
+    $text = preg_replace('#(\|)#isU', '</li><li>', $text);
+    return $text;
+  }
+
+  function parser_specialchars($text)
+  {
+    $text = preg_replace('#\\\\(.{1})#isU', '$1', $text);
+    return $text;
   }
 
   function form_test()
   {
-    $value = 'lalalal[ul|un||d[b|eu]x|[ul|trois]|qu[s|atr]e [[mot]]]
+    $text_test = 'lalalal[ul|un||d[b|eu]x|[ul|trois]|qu[s|atr]e [[mot]]]
               [bg|#dfd|avant[ol|a1|b2]après]
               [div|#ddf|
               [ol_|i|-5|x|y|z]
@@ -87,21 +153,11 @@
               [a| http://www.google.be | google.be]
               [a| http://www.google.be | google.be]';
 
-    $value2 = '[u|uuu[i|iii]bbb[i|iii] bbbb [i|iii]]';
+              $spechar= '\'"&<> \[\]\^\|';
     echo '<form name="testparse" action="index.php?page=wiki" method="post">
             <label> Votre texte : </label><br/>
-            <textarea rows="6" cols="50" name="text"> ' . $value2 . '</textarea><br/>
+            <textarea rows="6" cols="50" name="text"> ' . $text_test . '</textarea><br/>
             <input type="submit" name="submit"/>
           </form>';
   }
-
-  function parser_decode($text)
-  {
-    $text = preg_replace('#\[b\|(.+)\]#isU', '<b>$1</b>', $text);
-    $text = preg_replace('#\[u\|(.+)\]#isU', '<u>$1</u>', $text);
-    $text = preg_replace('#\[i\|(.+)\]#isU', '<i>$1</i>', $text);
-
-    return $text;
-  }
-
 ?>
