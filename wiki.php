fa56048121e29ff1754bc3bc08445df5b39048ae
Samuel Monroe <spat.monroe@gmail.com>
2015-05-13 23:27:02 +0200

    User can search for wiki's

--- wiki.php
+++ wiki.php
@@ -4,6 +4,11 @@
 
   form_test();
 
+  if(isset($_POST['search_wiki']))
+  {
+    search_wiki($_POST['title'], $_POST['description'], $_POST['keyword']);
+  }
+
   if(isset($_POST['submit']))
   {
     parser($_POST['text']);
