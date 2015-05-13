cbde04513ce796f2db7b0f48c7077414b3813baf
Samuel Monroe <spat.monroe@gmail.com>
2015-05-14 01:19:05 +0200

    Subject db insert function

--- functions/functions.wikisubjects.php
+++ functions/functions.wikisubjects.php
@@ -1,3 +1,19 @@
 <?php
 
+  function create_new_subject($title, $subject, $visibility_author, $userid)
+  {
+    $query = 'INSERT INTO subject(author_id, title, description, creation, visibility_author)
+              VALUES(:author_id, :title, :description, NOW(), :visibility_author);';
+
+    $result = $GLOBALS['dbsocket']->prepare($query);
+
+    $result->execute(array(
+      'author_id' => $userid,
+      'title' => $title,
+      'subject' => $subject,
+      'description' => $description,
+      'visibility_author' => $visibility_author
+    ));
+
+  }
 ?>
