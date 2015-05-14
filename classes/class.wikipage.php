486464c7b9e9539bae0f8e322759fa5bb44674c8
Samuel Monroe <spat.monroe@gmail.com>
2015-05-15 00:12:41 +0200

    Working on wiki page class

--- classes/class.wikipage.php
+++ classes/class.wikipage.php
@@ -2,5 +2,29 @@
 
   class Wikipage
   {
-    
+    private $id;
+		private $subject_id;
+		private $keyword;
+		private $content;
+		private $creation;
+		private $last_modification;
+
+    public function __construct($id)
+    {
+      $this->id = $id;
+
+      $query = 'SELECT subject_id, keyword, content, creation, last_modification
+                FROM page
+                WHERE id = \'' . $id . '\'';
+
+      $result = $GLOBALS['dbsocket']->query($query);
+
+      $wikipage = $result->fetch(PDO::FETCH_ASSOC);
+
+  		$this->subject_id = $wikipage['subject_id'];
+  		$this->keyword = $wikipage['keyword'];
+  		$this->content = $wikipage['content'];
+  		$this->creation = $wikipage['creation'];
+  		$this->last_modification = $wikipage['last_modification'];
+    }
   }
