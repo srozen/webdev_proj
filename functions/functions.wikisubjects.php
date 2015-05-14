fdc5cec5e36afc2ad6a26d72fa5c22724871f51d
Samuel Monroe <spat.monroe@gmail.com>
2015-05-14 09:48:12 +0200

    Wrong parameter and query preparing

--- functions/functions.wikisubjects.php
+++ functions/functions.wikisubjects.php
@@ -1,6 +1,6 @@
 <?php
 
-  function create_new_subject($title, $subject, $visibility_author, $userid)
+  function create_new_subject($title, $description, $visibility_author, $userid)
   {
     $query = 'INSERT INTO subject(author_id, title, description, creation, visibility_author)
               VALUES(:author_id, :title, :description, NOW(), :visibility_author);';
@@ -10,7 +10,6 @@
     $result->execute(array(
       'author_id' => $userid,
       'title' => $title,
-      'subject' => $subject,
       'description' => $description,
       'visibility_author' => $visibility_author
     ));
