6e40a56d3f0818712229e58994240551ef944128
Samuel Monroe <spat.monroe@gmail.com>
2015-05-15 00:15:07 +0200

    Creating owner boolean value

--- subject.php
+++ subject.php
@@ -2,12 +2,14 @@
 
   $subject = load_subject($_GET['subjectid']);
 
+  $owner = (logged() AND $subject->getAuthorId() == $_SESSION['user']->getId());
+
   if(isset($_POST['modify_subject']))
   {
     save_subject_modification($subject, $_POST['title'], $_POST['description'], $_POST['visibility_author']);
   }
 
-  if(isset($_GET['action']) AND $_GET['action'] == 'modifsubject' AND logged() AND $subject->getAuthorId() == $_SESSION['user']->getId())
+  if(isset($_GET['action']) AND $_GET['action'] == 'modifsubject' AND logged() AND $owner)
   {
     modify_subject_form($subject);
   }
@@ -23,6 +25,7 @@
     echo '<span><b> Modifié le : </b></span>' . $subject->getLastModification() . '<br/><br/>';
 
     echo '<h4> Pages du sujet : </h4>';
+    display_subject_pages($subject, $owner);
   }
 
 ?>
