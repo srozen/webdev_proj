943c1047f37561cd557c04bb52d7b848c7ca3e58
Samuel Monroe <spat.monroe@gmail.com>
2015-05-14 17:05:23 +0200

    Display subject from id in GET, user can juste browse it or modify if he is the author

---
+++ subject.php
@@ -0,0 +1,28 @@
+<?php
+
+  $subject = load_subject($_GET['subjectid']);
+
+  if(isset($_POST['modify_subject']))
+  {
+    save_subject_modification($subject, $_POST['title'], $_POST['description'], $_POST['visibility_author']);
+  }
+
+  if(isset($_GET['action']) AND $_GET['action'] == 'modifsubject' AND logged() AND $subject->getAuthorId() == $_SESSION['user']->getId())
+  {
+    modify_subject_form($subject);
+  }
+  else
+  {
+    if(logged() AND $subject->getAuthorId() == $_SESSION['user']->getId())
+    {
+      echo '<a href="index.php?page=subject&subjectid=' . $subject->getId() . '&action=modifsubject"> Modifier le sujet </a><br/>';
+    }
+    echo '<span><b> Titre : </b></span>' . $subject->getTitle() . '<br/>';
+    echo '<span><b> Auteur : </b></span>' . $subject->getAuthorName() .'<br/>';
+    echo '<span><b> Description : </b></span>' . $subject->getDescription() . '<br/>';
+    echo '<span><b> Modifié le : </b></span>' . $subject->getLastModification() . '<br/><br/>';
+
+    echo '<h4> Pages du sujet : </h4>';
+  }
+
+?>
