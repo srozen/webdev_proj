dd616ca0f5ef46aa4d8d1a4cb8cb0852ff7553fa
Samuel Monroe <spat.monroe@gmail.com>
2015-05-14 09:47:02 +0200

    User can create wiki subject and search engine display results

--- wiki.php
+++ wiki.php
@@ -5,6 +5,17 @@
   if(logged() AND !banned($_SESSION['user']->getId()) AND !frozen($_SESSION['user']->getId()) AND !unregistered($_SESSION['user']->getId()) AND !reactivating($_SESSION['user']->getId()))
   {
     create_wiki_form();
+    if(isset($_POST['create_wiki']))
+    {
+      if(filled($_POST['title'], $_POST['description'], $_POST['visibility_author']))
+      {
+        create_new_subject(sanitize($_POST['title']), sanitize($_POST['description']), sanitize($_POST['visibility_author']), $_SESSION['user']->getId());
+      }
+      else
+      {
+        echo '<div class="error_msg"> Les champs de création de sujet doivent être remplis ! </div>';
+      }
+    }
   }
 
   if(isset($_POST['search_wiki']))
