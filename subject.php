27ffbdc4b5327a1cd072e2ae079aba49644a8170
Samuel Monroe <spat.monroe@gmail.com>
2015-05-15 10:57:52 +0200

    Page can be displayed or included in modifying form

--- subject.php
+++ subject.php
@@ -1,8 +1,17 @@
 <?php
 
   $subject = load_subject($_GET['subjectid']);
+  if(isset($_GET['pageid']))
+  {
+    $page = load_page($_GET['pageid']);
+  }
+  else
+  {
+    $page = null;
+  }
 
   $owner = (logged() AND $subject->getAuthorId() == $_SESSION['user']->getId());
+  $page_related = page_related($subject, $page);
 
   if(isset($_POST['modify_subject']))
   {
@@ -15,17 +24,18 @@
   }
   else
   {
-    if(logged() AND $subject->getAuthorId() == $_SESSION['user']->getId())
-    {
-      echo '<a href="index.php?page=subject&subjectid=' . $subject->getId() . '&action=modifsubject"> Modifier le sujet </a><br/>';
-    }
-    echo '<span><b> Titre : </b></span>' . $subject->getTitle() . '<br/>';
-    echo '<span><b> Auteur : </b></span>' . $subject->getAuthorName() .'<br/>';
-    echo '<span><b> Description : </b></span>' . $subject->getDescription() . '<br/>';
-    echo '<span><b> Modifié le : </b></span>' . $subject->getLastModification() . '<br/><br/>';
-
-    echo '<h4> Pages du sujet : </h4>';
+    $subject->reload();
+    display_subject($subject);
     display_subject_pages($subject, $owner);
+  }
+
+  if(isset($_GET['action']) AND $_GET['action'] == 'displaypage' AND $page_related)
+  {
+    display_page($subject, $page);
+  }
+  if(isset($_GET['action']) AND $_GET['action'] == 'modifpage' AND $page_related AND $owner)
+  {
+    modify_page_form($subject, $page);
   }
 
 ?>
