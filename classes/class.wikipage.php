dd7b95ed21ffe5829dab280bd8104a825692c464
Samuel Monroe <spat.monroe@gmail.com>
2015-05-15 10:55:32 +0200

    Fully working wikipage class

--- classes/class.wikipage.php
+++ classes/class.wikipage.php
@@ -27,4 +27,123 @@
   		$this->creation = $wikipage['creation'];
   		$this->last_modification = $wikipage['last_modification'];
     }
+
+    public function reload()
+    {
+      $id = $this->getId();
+
+      $query = 'SELECT subject_id, keyword, content, creation, last_modification
+                FROM page
+                WHERE id = \'' . $id . '\'';
+
+      $result = $GLOBALS['dbsocket']->query($query);
+
+      $wikipage = $result->fetch(PDO::FETCH_ASSOC);
+
+      $this->setSubjectId($wikipage['subject_id']);
+      $this->setKeyword($wikipage['keyword']);
+      $this->setContent($wikipage['content']);
+      $this->setCreation($wikipage['creation']);
+      $this->setLastModification($wikipage['last_modification']);
+    }
+
+    public function update($field, $value)
+    {
+      $dbfield;
+      switch($field)
+      {
+        case 'subject_id' :
+          $dbfield = 'subject_id';
+          $this->setSubjectId($value);
+          break;
+
+        case 'keyword' :
+          $dbfield = 'keyword';
+          $this->setKeyword($value);
+          break;
+
+        case 'content' :
+          $dbfield = 'content';
+          $this->setContent($value);
+          break;
+
+        case 'creation' :
+          $dbfield = 'creation';
+          $this->setCreation($value);
+          break;
+
+        default:
+          $db_field = null;
+          break;
+      }
+
+      if($dbfield != null)
+      {
+        $query = 'UPDATE page
+                  SET ' . $dbfield . ' =\'' . $value . '\', last_modification = NOW()
+                  WHERE id=\'' . $this->getId() . '\';';
+        $GLOBALS['dbsocket']->exec($query);
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+
+    public function getId()
+    {
+      return $this->id;
+    }
+
+    public function setSubjectId($id)
+    {
+      $this->subject_id=$id;
+    }
+
+    public function getSubjectId()
+    {
+      return $this->subject_id;
+    }
+
+    public function setKeyword($keyword)
+    {
+      $this->keyword=$keyword;
+    }
+
+    public function getKeyword()
+    {
+      return $this->keyword;
+    }
+
+    public function setContent($content)
+    {
+      $this->content=$content;
+    }
+
+    public function getContent()
+    {
+      return $this->content;
+    }
+
+    public function setCreation($creation)
+    {
+      $this->creation=$creation;
+    }
+
+    public function getCreation()
+    {
+      return $this->creation;
+    }
+
+    public function setLastModification($last_modification)
+    {
+      $this->last_modification=$last_modification;
+    }
+
+    public function getLastModification()
+    {
+      return $this->last_modification;
+    }
   }
