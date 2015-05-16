9c930626542ee7b00fb4145a8eb3799040ac7892
Samuel Monroe <spat.monroe@gmail.com>
2015-05-16 22:33:24 +0200

    New pages functions and keyword checker for the parser

--- functions/functions.wikipage.php
+++ functions/functions.wikipage.php
@@ -1,15 +1,29 @@
 <?php
 
-  function create_start_page($subject_id)
+  function create_start_page($subject_id, $start = false)
   {
-      $query = 'INSERT INTO page(subject_id, creation)
-                VALUES(:subject_id, NOW());';
+      $query = 'INSERT INTO page(subject_id, creation, start)
+                VALUES(:subject_id, NOW(), :start);';
 
       $result = $GLOBALS['dbsocket']->prepare($query);
 
       $result->execute(array(
-        'subject_id' => $subject_id
+        'subject_id' => $subject_id,
+        'start' => $start
       ));
+  }
+
+  function create_page_form($subject, $keyword = '')
+  {
+    echo '<a href="index.php?page=subject&subjectid=' . $subject->getId() . '&action=displaypage"> Retour au sujet </a><br/>
+          <form name="create_page" action="index.php?page=subject&subjectid=' . $subject->getId() .'&action=createpage" method="post">
+            <h3> Création d\'une page : </h3>
+            <label> Mot-clé : </label><br/>
+              <input type="text" name="keyword" value="' . $keyword . '" /><br/>
+            <label> Contenu : </label><br/>
+              <textarea rows="6" cols="50" name="content"></textarea><br/>
+            <input type="submit" name="create_page"/>
+          </form>';
   }
 
   function display_page($subject, $page)
@@ -21,27 +35,58 @@
     }
     echo '<span><b> Mot clé : </b></span><br/>' . $page->getKeyword() . '<br/>';
     echo '<span><b> Modifié le : </b></span><br/>' . $page->getLastModification() . '<br/>';
-    echo '<span><b> Contenu : </b></span><br/>' . parser($page->getContent()) . '<br/><br/>';
+    echo '<span><b> Contenu : </b></span><br/>' . parser($page->getContent(), $subject) . '<br/><br/>';
   }
 
   function modify_page_form($subject, $page)
   {
+
     echo '<a href="index.php?page=subject&subjectid=' . $subject->getId() . '&action=displaypage&pageid=' . $page->getId() . '"> Retour à la page </a><br/>
           <form name="create_page" action="index.php?page=subject&subjectid=' . $subject->getId() .'&action=displaypage&pageid=' . $page->getId() . '" method="post">
             <h3> Modification de la page : </h3>
             <label> Mot-clé : </label><br/>
-              <input type="text" name="keyword" value="' . $page->getKeyword() .'" /><br/>
+              <input type="text" name="keyword" value="' . $page->getKeyword() . '" /><br/>
             <label> Contenu : </label><br/>
               <textarea rows="6" cols="50" name="content">' . $page->getContent() .'</textarea><br/>
             <input type="submit" name="modify_page"/>
           </form>';
   }
 
+  function save_new_page($subject, $keyword, $content)
+  {
+    if(filled($keyword) AND filled($content))
+    {
+      if(!keyword_exists($subject, $keyword))
+      {
+        $query = 'INSERT INTO page(subject_id, keyword, content, creation, start)
+                  VALUES(:subject_id, :keyword, :content, NOW(), false);';
+        $result = $GLOBALS['dbsocket']->prepare($query);
+
+        $result->execute(array(
+          'subject_id' => $subject->getId(),
+          'keyword' => $keyword,
+          'content' => $content
+        ));
+      }
+      else
+      {
+        echo '<div class="error_msg"> Le mot clé existe déjà !! </div>';
+      }
+    }
+    else
+    {
+      echo '<div class="error_msg"> Tout les champs ne sont pas remplis !! </div>';
+    }
+  }
+
   function save_page_modification($page, $keyword, $content)
   {
     if(filled($keyword) AND $keyword != $page->getKeyword())
     {
-      $page->update('keyword', $keyword);
+      if($page->getStart() == false)
+      {
+        $page->update('keyword', $keyword);
+      }
     }
     if(filled($content) AND $content != $page->getContent())
     {
@@ -97,6 +142,24 @@
     $query = 'SELECT count(*)
               FROM page
               WHERE ' . $field . ' = \'' . $value . '\';';
+
+    $result = $GLOBALS['dbsocket']->query($query);
+
+    if($result->fetchColumn() > 0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  function keyword_exists($subject, $keyword)
+  {
+    $query = 'SELECT count(*)
+              FROM page
+              WHERE keyword = \'' . $keyword . '\' AND subject_id = ' . $subject->getId() . ' ;';
 
     $result = $GLOBALS['dbsocket']->query($query);
 
