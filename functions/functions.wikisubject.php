cf52ca9602153db74ccd430db36ffbe50f0b17a7
Samuel Monroe <spat.monroe@gmail.com>
2015-05-16 22:33:46 +0200

    Edited menu generation depending on the user level

--- functions/functions.wikisubject.php
+++ functions/functions.wikisubject.php
@@ -15,22 +15,22 @@
     ));
 
     $subject_id = get_subject_value('id', 'title', $title);
-    create_start_page($subject_id);
+    create_start_page($subject_id, true);
 
     $query = 'SELECT id FROM page WHERE subject_id = ' . $subject_id . ' AND keyword is null';
     $result = $GLOBALS['dbsocket']->query($query);
     $page = $result->fetch();
 
-    header('Location: index.php?page=subject&subjectid=' . $subject_id . '&modifpage=' . $page['id']);
+    header('Location: index.php?page=subject&subjectid=' . $subject_id . '&pageid=' . $page['id'] . '&action=modifpage');
 
   }
 
-  function display_subject($subject)
+  function display_subject($subject, $authorized)
   {
-    if(logged() AND $subject->getAuthorId() == $_SESSION['user']->getId())
+    if(logged() AND $authorized)
     {
       echo '<a href="index.php?page=subject&subjectid=' . $subject->getId() . '&action=modifsubject"> Modifier le sujet </a><br/>';
-      echo '<a href="index.php?page=subject&subjectid=' . $subject->getId() . '"> Ajouter une page</a><br/>';
+      echo '<a href="index.php?page=subject&subjectid=' . $subject->getId() . '&action=createpage"> Ajouter une page</a><br/>';
     }
     echo '<span><b> Titre : </b></span>' . $subject->getTitle() . '<br/>';
     echo '<span><b> Auteur : </b></span>' . $subject->getAuthorName() .'<br/>';
@@ -70,15 +70,38 @@
               <input type="text" name="title" value="' . $subject->getTitle() .'" /><br/>
             <label> Description du sujet : </label><br/>
               <textarea rows="6" cols="50" name="description">' . $subject->getDescription() .'</textarea><br/>
-            <label> Choix de visibilité : </label>
-              <select name="visibility_author">
-                <option value="0" '; if($subject->getVisibilityAuthor() == 0) echo 'selected'; echo '> Pas de choix </option>
+            <label> Choix de visibilité : </label><br>
+            <label> Choix de visibilité utilisateur: </label><select name="visibility_author">
+                <option value="0" '; if($subject->getVisibilityAuthor() == 0) echo 'selected'; echo'> Pas de choix </option>
                 <option value="1" '; if($subject->getVisibilityAuthor() == 1) echo 'selected'; echo'> Anonymes </option>
                 <option value="2" '; if($subject->getVisibilityAuthor() == 2) echo 'selected'; echo'> Utilisateurs </option>';
-                if(moderator_level($_SESSION['user']->getId())) echo '<option value="3"> Modérateurs </option>';
-                if(admin_level($_SESSION['user']->getId())) echo '<option value="4"> Administrateur </option>';
-        echo '</select><br/>
-            <input type="submit" name="modify_subject"/>
+                if(moderator_level($_SESSION['user']->getId())) echo '<option value="3" '; if($subject->getVisibilityAuthor() == 3) echo 'selected'; echo'> Modérateurs </option>';
+                if(admin_level($_SESSION['user']->getId())) echo '<option value="4" '; if($subject->getVisibilityAuthor() == 4) echo 'selected'; echo'> Administrateur </option>
+              </select><br/>';
+
+        if($subject->getModerator() == $_SESSION['user']->getId() OR admin_level($_SESSION['user']->getId()))
+        {
+          echo '<label> Choix de visibilité modérateur: </label><select name="visibility_modo">
+                <option value="0" '; if($subject->getVisibilityModo() == 0) echo 'selected'; echo '> Pas de choix </option>
+                <option value="1" '; if($subject->getVisibilityModo() == 1) echo 'selected'; echo'> Anonymes </option>
+                <option value="2" '; if($subject->getVisibilityModo() == 2) echo 'selected'; echo'> Utilisateurs </option>
+                <option value="3" '; if($subject->getVisibilityModo() == 3) echo 'selected'; echo'> Modérateurs </option>';
+                if(admin_level($_SESSION['user']->getId())) echo '<option value="4" '; if($subject->getVisibilityModo() == 4) echo 'selected'; echo'> Administrateur </option>
+              </select><br/>';
+        }
+
+        if(admin_level($_SESSION['user']->getId()))
+        {
+          echo '<label> Choix de visibilité administrateur: </label><select name="visibility_admin">
+                <option value="0" '; if($subject->getVisibilityAdmin() == 0) echo 'selected'; echo '> Pas de choix </option>
+                <option value="1" '; if($subject->getVisibilityAdmin() == 1) echo 'selected'; echo'> Anonymes </option>
+                <option value="2" '; if($subject->getVisibilityAdmin() == 2) echo 'selected'; echo'> Utilisateurs </option>
+                <option value="3" '; if($subject->getVisibilityAdmin() == 3) echo 'selected'; echo'> Modérateurs </option>
+                <option value="4" '; if($subject->getVisibilityAdmin() == 4) echo 'selected'; echo'> Administrateur </option>
+              </select><br/>';
+        }
+
+        echo '<input type="submit" name="modify_subject"/>
           </form>';
   }
 
@@ -105,7 +128,7 @@
       foreach($pages as $page)
       {
         echo '<tr>';
-          echo '<td><a href="index.php?page=subject&subjectid=' . $subject->getId() . '&pageid=' . $page['id'] . '&action=displaypage">'; if(isset($page['Mot-clé'])) echo $page['Mot-clé']; else echo 'Page d\'entrée'; echo '</a></td>';
+          echo '<td><a href="index.php?page=subject&subjectid=' . $subject->getId() . '&pageid=' . $page['id'] . '&action=displaypage">'; if(isset($page['Mot-Clé'])) echo $page['Mot-Clé']; else echo 'Page d\'entrée'; echo '</a></td>';
           echo '<td>' . $page['Date de création'] . '</td>';
           echo '<td>'; if(isset($page['Dernière modification'])) echo $page['Dernière modification']; else echo '---'; echo '</td>';
         echo '</tr>';
@@ -117,7 +140,7 @@
 
 
 
-  function save_subject_modification($subject, $title, $description, $visibility_author)
+  function save_subject_modification($subject, $title, $description, $visibility_author, $visibility_modo, $visibility_admin)
   {
     if(filled($title) AND $title != $subject->getTitle())
     {
@@ -131,6 +154,15 @@
     {
       $subject->update('visibility_author', $visibility_author);
     }
+    if(filled($visibility_modo) AND $visibility_modo != $subject->getVisibilityModo() AND (($subject->getModerator() == $_SESSION['user']->getId()) OR (admin_level($_SESSION['user']->getId()))))
+    {
+      $subject->update('visibility_modo', $visibility_modo);
+    }
+    if(filled($visibility_admin) AND $visibility_admin != $subject->getVisibilityAdmin() AND admin_level($_SESSION['user']->getId()))
+    {
+      $subject->update('visibility_admin', $visibility_admin);
+    }
+
   }
 
 
@@ -144,9 +176,11 @@
   */
   function get_subject_value($value, $col, $colvalue)
   {
+    $colvalue = $GLOBALS['dbsocket']->quote($colvalue);
     $query = 'SELECT ' . $value . '
               FROM subject
-              WHERE ' . $col . ' = \'' . $colvalue . '\';';
+              WHERE ' . $col . ' = ' . $colvalue . ';';
+
 
     $result = $GLOBALS['dbsocket']->query($query);
     $subject = $result->fetch();
